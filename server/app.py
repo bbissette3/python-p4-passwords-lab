31e4a3ded876932c179e0db4a1e12a30e26648b8
Brian Bissette <bbissette3@yahoo.com>
2023-04-28 13:54:43 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -16,17 +16,56 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username= data['username']
+        password = data['password']
+
+        if username and password:
+            user = User(username=username)
+            user.password_hash = password
+
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+
+            return user.to_dict(), 201
+    
+api.add_resource(Signup, '/signup')
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        
+        return {}, 204
+        
+api.add_resource(CheckSession, '/check_session')
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+
+            return user.to_dict(), 200
+
+api.add_resource(Login, '/login')
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
+api.add_resource(Logout, '/logout')
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 
 if __name__ == '__main__':
